b4f4d7d893de86af449193883fe05e9e5b0f3deb
Matheus Perrut <matheus.perrut2302@gmail.com>
2019-09-10 01:32:49 -0300

    Adição de Splash Screen e correção da arquitetura de arquivos

--- src/services/subcategorias-api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const subcategoriasResource = axios.create({
-  baseURL: 'https://finddo-back-dev.herokuapp.com/subcategories',
-});
-
-export default subcategoriasResource;
