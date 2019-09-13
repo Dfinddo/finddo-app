5cbee066547b8dd384ff2d235751a247468e00f9
Matheus Perrut <matheus.perrut2302@gmail.com>
2019-09-13 04:15:28 -0300

    Correção path api

--- src/services/backend-rails-api.js
+++ src/services/backend-rails-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const backendRails = axios.create({
-  baseURL: 'http://192.168.1.7:3000',
+  baseURL: 'https://finddo-back-dev.herokuapp.com',
 });
 
 export default backendRails;
