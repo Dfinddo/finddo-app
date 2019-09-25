4ca51426e26f0be7c3f5991cc01185d344db1577
Matheus Perrut <matheus.perrut2302@gmail.com>
2019-09-24 22:52:11 -0300

    Build de release

--- src/services/backend-rails-api.js
+++ src/services/backend-rails-api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const backendRails = axios.create({
-  baseURL: 'http://192.168.56.1:3000',
-  // baseURL: 'https://finddo-back-dev.herokuapp.com',
+  // baseURL: 'http://192.168.56.1:3000',
+  baseURL: 'https://finddo-back-dev.herokuapp.com',
 });
 
 export default backendRails;
