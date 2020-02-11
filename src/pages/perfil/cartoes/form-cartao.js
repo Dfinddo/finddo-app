26fd8a030ba030ba7099e83b9287c8826d33b4bf
Matheus Perrut <matheus.perrut2302@gmail.com>
2020-02-11 01:50:08 -0300

    - Formulário de Criação de Cartão Adicionado

--- src/pages/perfil/cartoes/form-cartao.js
+++ src/pages/perfil/cartoes/form-cartao.js
@@ -12,6 +12,32 @@ import backendRails from '../../../services/backend-rails-api';
 import TokenService from '../../../services/token-service';
 import HeaderFundoTransparente from '../../../components/header-fundo-transparente';
 import { StackActions } from 'react-navigation';
+import { headers } from '../../../services/moip-api';
+
+function Card() {
+  return {
+    method: 'CREDIT_CARD',
+    creditCard: {
+      expirationMonth: '',
+      expirationYear: '',
+      number: '',
+      cvc: '',
+      holder: {
+        fullname: '',
+        birthdate: '',
+        taxDocument: {
+          type: 'CPF',
+          number: ''
+        },
+        phone: {
+          countryCode: '55',
+          areaCode: '',
+          number: ''
+        }
+      }
+    }
+  };
+}
 
 function Item({ title }) {
   return (
@@ -28,17 +54,8 @@ export default class FormCartaoScreen extends Component {
   };
 
   state = {
-    tituloForm: '',
-    id: null,
-    nome: '',
-    cep: '',
-    estado: 'RJ',
-    cidade: 'Rio de Janeiro',
-    bairro: '',
-    rua: '',
-    numero: '',
-    complemento: '',
-    selected: false,
+    tituloForm: 'Adicionar Cartão',
+    cardData: Card(),
     formInvalid: false,
     formErrors: [],
     isLoading: false,
@@ -54,33 +71,6 @@ export default class FormCartaoScreen extends Component {
       'keyboardDidHide',
       this._keyboardDidHide,
     );
-
-    const { navigation } = this.props;
-    const endereco = navigation.getParam('endereco', null);
-    const editar = navigation.getParam('editar', null);
-
-    if (editar) {
-      this.setState({
-        tituloForm: 'Editar Endereço'
-      });
-    } else {
-      this.setState({
-        tituloForm: 'Adicionar Endereço'
-      });
-    }
-
-    if (endereco) {
-      this.setState({
-        id: endereco.id,
-        nome: endereco.name,
-        cep: endereco.cep,
-        bairro: endereco.district,
-        rua: endereco.street,
-        numero: endereco.number,
-        complemento: endereco.complement,
-        selected: endereco.selected,
-      });
-    }
   };
 
   _keyboardDidShow = () => {
@@ -91,144 +81,54 @@ export default class FormCartaoScreen extends Component {
     this.setState({ isShowingKeyboard: false });
   }
 
-  obterEndereco = () => {
-    const tokenService = TokenService.getInstance();
-    const address = {};
-
-    if (this.state.id) {
-      address['id'] = this.state.id;
-    }
-    address['name'] = this.state.nome;
-    address['cep'] = this.state.cep;
-    address['district'] = this.state.bairro;
-    address['street'] = this.state.rua;
-    address['number'] = this.state.numero;
-    address['complement'] = this.state.complemento;
-    address['selected'] = this.state.selected;
-    address['user_id'] = tokenService.getUser().id;
-
-    return address;
-  }
-
   componentWillUnmount() {
     this.keyboardDidShowListener.remove();
     this.keyboardDidHideListener.remove();
   }
 
-  validateFields = () => {
-    const numberRegex = /^[0-9]*$/;
+  atualizarDadosCartao = (chave, valor) => {
+    const card = this.state.cardData;
+    card.creditCard[chave] = valor;
 
-    const errosArr = [];
-
-    const nomeErrors = [];
-    const cepErrors = [];
-    const estadoErrors = [];
-    const cidadeErrors = [];
-    const bairroErrors = [];
-    const ruaErrors = [];
-    const numeroErrors = [];
-    const complementoErrors = [];
-
-    if (this.state.cep.length === 0) {
-      cepErrors.push('É obrigatório.');
-    } else if (this.state.cep.length > 10) {
-      cepErrors.push('Tamanho máximo 10.');
-    } else if (!numberRegex.test(this.state.cep)) {
-      cepErrors.push('Apenas números.');
-    }
-
-    if (this.state.estado.length === 0) {
-      estadoErrors.push('É obrigatório.');
-    } else if (this.state.estado.length > 2) {
-      estadoErrors.push('Tamanho máximo 2.');
-    }
-
-    if (this.state.nome.length === 0) {
-      nomeErrors.push('É obrigatório.');
-    } else if (this.state.cidade.length > 128) {
-      nomeErrors.push('Tamanho máximo 128.');
-    }
-
-    if (this.state.cidade.length === 0) {
-      cidadeErrors.push('É obrigatório.');
-    } else if (this.state.cidade.length > 128) {
-      cidadeErrors.push('Tamanho máximo 128.');
-    }
-
-    if (this.state.bairro.length === 0) {
-      bairroErrors.push('É obrigatório.');
-    } else if (this.state.cidade.length > 128) {
-      bairroErrors.push('Tamanho máximo 128.');
-    }
-
-    if (this.state.rua.length === 0) {
-      ruaErrors.push('É obrigatório.');
-    } else if (this.state.rua.length > 128) {
-      ruaErrors.push('Tamanho máximo 128.');
-    }
-
-    if (this.state.numero.length === 0) {
-      numeroErrors.push('É obrigatório.');
-    } else if (this.state.numero.length > 10) {
-      numeroErrors.push('Tamanho máximo 10.');
-    }
-
-    if (this.state.complemento.length === 0) {
-      complementoErrors.push('É obrigatório.');
-    } else if (this.state.complemento.length > 128) {
-      complementoErrors.push('Tamanho máximo 128.');
-    }
-
-    if (nomeErrors.length > 0) {
-      errosArr.push({ title: 'Nome do endereço', data: nomeErrors });
-    }
-    if (cepErrors.length > 0) {
-      errosArr.push({ title: 'CEP', data: cepErrors });
-    }
-    if (estadoErrors.length > 0) {
-      errosArr.push({ title: 'Estado', data: estadoErrors });
-    }
-    if (cidadeErrors.length > 0) {
-      errosArr.push({ title: 'Cidade', data: cidadeErrors });
-    }
-    if (bairroErrors.length > 0) {
-      errosArr.push({ title: 'Bairro', data: bairroErrors });
-    }
-    if (ruaErrors.length > 0) {
-      errosArr.push({ title: 'Rua', data: ruaErrors });
-    }
-    if (numeroErrors.length > 0) {
-      errosArr.push({ title: 'Número', data: numeroErrors });
-    }
-    if (complementoErrors.length > 0) {
-      errosArr.push({ title: 'Complemento', data: complementoErrors });
-    }
-
-    if (errosArr.length > 0) {
-      this.setState({ formErrors: [...errosArr] });
-      this.setState({ formInvalid: true });
-    } else {
-      this.salvarEndereco();
-      // this.props.navigation.navigate('ParteDois', this.state);
-    }
+    this.setState({ cardData: card });
   };
 
-  salvarEndereco = async () => {
-    const address = this.obterEndereco();
-    const tokenService = TokenService.getInstance();
+  atualizarDadosHolder = (chave, valor) => {
+    const card = this.state.cardData;
+    card.creditCard.holder[chave] = valor;
 
+    this.setState({ cardData: card });
+  };
+
+  atualizarDadosHolderDocs = (chave, valor) => {
+    const card = this.state.cardData;
+    card.creditCard.holder.taxDocument[chave] = valor;
+
+    this.setState({ cardData: card });
+  };
+
+  atualizarDadosHolderPhone = (chave, valor) => {
+    const card = this.state.cardData;
+    card.creditCard.holder.phone[chave] = valor;
+
+    this.setState({ cardData: card });
+  };
+
+  validateCard = () => {
+    setTimeout(() => console.log(JSON.stringify(this.state.cardData)), 2000);
+  };
+
+  salvarCartao = async () => {
     try {
       this.setState({ isLoading: true });
-      const response = this.state.id
-        ? await backendRails.put(`/addresses/${address.id}`, { address }, { headers: tokenService.getHeaders() })
-        : await backendRails.post('/addresses', { address }, { headers: tokenService.getHeaders() });
+      const response = await backendRails.post('/addresses', { address }, { headers: headers });
 
       this.setState({ isLoading: false, id: response.data.id });
 
       const popAction = StackActions.pop({
         n: 1,
       });
-      this.props.navigation.dispatch(popAction);
+      // this.props.navigation.dispatch(popAction);
     }
     catch (error) {
       if (error.response) {
@@ -269,7 +169,7 @@ export default class FormCartaoScreen extends Component {
     // TODO: tipo inputs aqui e no login form
     return (
       <ImageBackground
-        style={this.formEnderecoScreenStyle.backgroundImageContent}
+        style={this.formCartaoScreenStyle.backgroundImageContent}
         source={require('../../../img/Ellipse.png')} >
         <View style={{ height: 60 }}></View>
         <ScrollView>
@@ -278,7 +178,7 @@ export default class FormCartaoScreen extends Component {
             transparent={true}
             visible={this.state.isLoading}
           >
-            <View style={this.formEnderecoScreenStyle.modalStyle}>
+            <View style={this.formCartaoScreenStyle.modalStyle}>
               <View>
                 <ActivityIndicator size="large" color={colors.verdeFinddo} animating={true} />
               </View>
@@ -289,93 +189,96 @@ export default class FormCartaoScreen extends Component {
             transparent={true}
             visible={this.state.formInvalid}
           >
-            <View style={this.formEnderecoScreenStyle.modalBase}>
-              <View style={this.formEnderecoScreenStyle.modalDialog}>
-                <View style={this.formEnderecoScreenStyle.modalDialogContent}>
-                  <Text style={this.formEnderecoScreenStyle.modalErrosTitulo}>Erros:</Text>
+            <View style={this.formCartaoScreenStyle.modalBase}>
+              <View style={this.formCartaoScreenStyle.modalDialog}>
+                <View style={this.formCartaoScreenStyle.modalDialogContent}>
+                  <Text style={this.formCartaoScreenStyle.modalErrosTitulo}>Erros:</Text>
                   <SectionList
-                    style={this.formEnderecoScreenStyle.modalErrosSectionList}
+                    style={this.formCartaoScreenStyle.modalErrosSectionList}
                     sections={this.state.formErrors}
                     keyExtractor={(item, index) => item + index}
                     renderItem={({ item }) => <Item title={item} />}
                     renderSectionHeader={({ section: { title } }) => (
-                      <Text style={this.formEnderecoScreenStyle.modalErrosTituloErro}>{title}</Text>
+                      <Text style={this.formCartaoScreenStyle.modalErrosTituloErro}>{title}</Text>
                     )}
                   />
                   <TouchableOpacity
-                    style={this.formEnderecoScreenStyle.modalErrosBotaoContinuar}
+                    style={this.formCartaoScreenStyle.modalErrosBotaoContinuar}
                     onPress={() => this.setState({ formInvalid: false })}>
-                    <Text style={this.formEnderecoScreenStyle.continuarButtonText}>VOLTAR</Text>
+                    <Text style={this.formCartaoScreenStyle.continuarButtonText}>VOLTAR</Text>
                   </TouchableOpacity>
                 </View>
               </View>
             </View>
           </Modal>
-          <View style={this.formEnderecoScreenStyle.cadastroForm}>
-            <View style={this.formEnderecoScreenStyle.cadastroMainForm}>
-              <Text style={this.formEnderecoScreenStyle.fontTitle}>{this.state.tituloForm}</Text>
+          <View style={this.formCartaoScreenStyle.cadastroForm}>
+            <View style={this.formCartaoScreenStyle.cadastroMainForm}>
+              <Text style={this.formCartaoScreenStyle.fontTitle}>{this.state.tituloForm}</Text>
 
               <TextInput
-                style={this.formEnderecoScreenStyle.cadastroFormSizeAndFont}
-                onChangeText={text => { this.setState({ nome: text }) }}
-                placeholder="Nome do endereço"
-                value={this.state.nome}
+                style={this.formCartaoScreenStyle.cadastroFormSizeAndFont}
+                onChangeText={text => { this.atualizarDadosCartao('expirationMonth', text) }}
+                placeholder="Expiração MM" keyboardType={'number-pad'}
+                value={this.state.cardData.creditCard.expirationMonth}
               />
               <TextInput
-                style={this.formEnderecoScreenStyle.cadastroFormSizeAndFont}
-                onChangeText={text => { this.setState({ cep: text }) }}
-                placeholder="CEP"
-                keyboardType={'number-pad'}
-                value={this.state.cep}
+                style={this.formCartaoScreenStyle.cadastroFormSizeAndFont}
+                onChangeText={text => { this.atualizarDadosCartao('expirationYear', text) }}
+                placeholder="Expiração AAAA" keyboardType={'number-pad'}
+                value={this.state.cardData.creditCard.expirationYear}
               />
               <TextInput
-                style={this.formEnderecoScreenStyle.cadastroFormSizeAndFont}
-                onChangeText={text => { this.setState({ estado: text }) }}
-                placeholder="Estado"
-                editable={false}
-                value={this.state.estado}
+                style={this.formCartaoScreenStyle.cadastroFormSizeAndFont}
+                onChangeText={text => { this.atualizarDadosCartao('number', text) }}
+                placeholder="Número" keyboardType={"number-pad"}
+                value={this.state.cardData.creditCard.number}
               />
               <TextInput
-                style={this.formEnderecoScreenStyle.cadastroFormSizeAndFont}
-                onChangeText={text => { this.setState({ cidade: text }) }}
-                placeholder="Cidade"
-                editable={false}
-                value={this.state.cidade}
+                style={this.formCartaoScreenStyle.cadastroFormSizeAndFont}
+                onChangeText={text => { this.atualizarDadosCartao('cvc', text) }}
+                placeholder="cvc" keyboardType={"number-pad"}
+                value={this.state.cardData.creditCard.cvc}
               />
               <TextInput
-                style={this.formEnderecoScreenStyle.cadastroFormSizeAndFont}
-                onChangeText={text => { this.setState({ bairro: text }) }}
-                placeholder="Bairro"
-                value={this.state.bairro}
+                style={this.formCartaoScreenStyle.cadastroFormSizeAndFont}
+                onChangeText={text => { this.atualizarDadosHolder('fullname', text) }}
+                placeholder="Titular (Nome completo)"
+                value={this.state.cardData.creditCard.holder.fullname}
               />
               <TextInput
-                style={this.formEnderecoScreenStyle.cadastroFormSizeAndFont}
-                onChangeText={text => { this.setState({ rua: text }) }}
-                placeholder="Rua"
-                value={this.state.rua}
+                style={this.formCartaoScreenStyle.cadastroFormSizeAndFont}
+                onChangeText={text => { this.atualizarDadosHolder('birthdate', text) }}
+                placeholder="Data nascimento (dd/mm/aaaa)" keyboardType={"numbers-and-punctuation"}
+                value={this.state.cardData.creditCard.holder.birthdate}
               />
               <TextInput
-                style={this.formEnderecoScreenStyle.cadastroFormSizeAndFont}
-                onChangeText={text => { this.setState({ numero: text }) }}
-                placeholder="Número"
-                value={this.state.numero}
+                style={this.formCartaoScreenStyle.cadastroFormSizeAndFont}
+                onChangeText={text => { this.atualizarDadosHolderDocs('number', text) }}
+                placeholder="CPF" keyboardType={"number-pad"}
+                value={this.state.cardData.creditCard.holder.taxDocument.number}
               />
               <TextInput
-                style={this.formEnderecoScreenStyle.cadastroFormSizeAndFont}
-                onChangeText={text => { this.setState({ complemento: text }) }}
-                placeholder="Complemento"
-                value={this.state.complemento}
+                style={this.formCartaoScreenStyle.cadastroFormSizeAndFont}
+                onChangeText={text => { this.atualizarDadosHolderPhone('areaCode', text) }}
+                placeholder="DDD" keyboardType={"number-pad"}
+                value={this.state.cardData.creditCard.holder.phone.areaCode}
+              />
+              <TextInput
+                style={this.formCartaoScreenStyle.cadastroFormSizeAndFont}
+                onChangeText={text => { this.atualizarDadosHolderPhone('number', text) }}
+                placeholder="Telefone" keyboardType={"number-pad"}
+                value={this.state.cardData.creditCard.holder.phone.number}
               />
               <View style={{ height: 8 }}></View>
             </View>
           </View>
         </ScrollView>
-        <BotaoCriar isShowingKeyboard={this.state.isShowingKeyboard} onPress={() => this.validateFields()}></BotaoCriar>
+        <BotaoCriar isShowingKeyboard={this.state.isShowingKeyboard} onPress={() => this.validateCard()}></BotaoCriar>
       </ImageBackground>
     );
   }
 
-  formEnderecoScreenStyle = StyleSheet.create({
+  formCartaoScreenStyle = StyleSheet.create({
     backgroundImageContent: { width: '100%', height: '100%' },
     cadastroForm: {
       flex: 1, alignItems: 'center',
