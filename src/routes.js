cde318df09329e5d074d6bf72f10c7463f3235ab
Matheus Perrut <matheus.perrut2302@gmail.com>
2020-02-16 17:24:12 -0300

    - Adição da dependência react-native-onesignal - Adição de scripts de teste para validação das notificações

--- src/routes.js
+++ src/routes.js
@@ -31,6 +31,7 @@ import FormEnderecoScreen from './pages/perfil/form-endereco';
 import CartoesScreen from './pages/perfil/cartoes/cartoes';
 import FormCartaoScreen from './pages/perfil/cartoes/form-cartao';
 import ValorServicoScreen from './pages/acompanhamento-finddo-pay/tela-valor';
+import OneSignal from 'react-native-onesignal';
 
 const AppStack = createStackNavigator(
   {
@@ -169,6 +170,38 @@ const AppContainer = createAppContainer(createSwitchNavigator(
 ));
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    // ============================ ATENÇÃO ============================
+    // ================ NUNCA COMMITAR O ID DA APLICAÇÃO ===============
+    OneSignal.init("APPLICATION ID");
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
+
   render() {
     return <AppContainer />;
   }
