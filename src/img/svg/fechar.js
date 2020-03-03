853c74344fd07628f70c454e9969ee60a6547bc8
Matheus Perrut <matheus.perrut2302@gmail.com>
2020-03-03 01:28:09 -0300

    - Inclusão da tela de Tutorial

---
+++ src/img/svg/fechar.js
@@ -0,0 +1,5 @@
+export const fechar = `
+<svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M2.30292 2.0046L17 16.7017M16.6971 2L2 16.6971" stroke="#B00F00" stroke-width="3"/>
+</svg>
+`;
--- src/pages/servicos/servicos.js
+++ src/pages/servicos/servicos.js
@@ -3,6 +3,15 @@ import { Modal, Image, ImageBackground, Text, FlatList, StyleSheet, TouchableOpa
 import moipAPI, { headers } from '../../services/moip-api';
 import { colors } from '../../colors';
 import TokenService from '../../services/token-service';
+import { SvgXml } from 'react-native-svg';
+import { bolaCheia } from '../../img/svg/bola-cheia';
+import { bolaApagada } from '../../img/svg/bola-apagada';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { passo0 } from '../../img/svg/passo-0';
+import { passo1 } from '../../img/svg/passo-1';
+import { passo2 } from '../../img/svg/passo-2';
+import { passo3 } from '../../img/svg/passo-3';
+import { fechar } from '../../img/svg/fechar';
 
 export default class Servicos extends Component {
   static navigationOptions = {
@@ -21,7 +30,9 @@ export default class Servicos extends Component {
       { id: '8', name: null }
     ],
     page: 1,
-    isLoading: false
+    isLoading: false,
+    isShowingTutorial: true,
+    currentTutorialStep: 0
   };
 
   creditCardFilter = (data) => {
@@ -98,6 +109,189 @@ export default class Servicos extends Component {
             <View style={this.servicosStyles.modalStyle}>
               <View>
                 <ActivityIndicator size="large" color={colors.verdeFinddo} animating={true} />
+              </View>
+            </View>
+          </Modal>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={this.state.isShowingTutorial}
+          >
+            <View style={{
+              flex: 1, backgroundColor: 'rgba(255, 255, 255, 0.2)',
+              alignItems: 'center', justifyContent: 'center'
+            }}>
+              <View
+                style={{
+                  width: 320, height: 520,
+                  backgroundColor: colors.branco, borderRadius: 20,
+                  alignItems: 'center', justifyContent: 'center'
+                }}>
+                <TouchableOpacity
+                  onPress={() => this.setState({ isShowingTutorial: false })}
+                  style={{ marginHorizontal: 10, top: 10, left: 280, position: 'absolute' }}>
+                  <SvgXml
+                    xml={fechar}
+                    width={20} height={20}></SvgXml>
+                </TouchableOpacity>
+                <View style={{ height: 20 }}></View>
+                <View style={{
+                  height: 100, width: '100%',
+                  alignItems: 'center', justifyContent: 'center'
+                }}>
+                  {(() => {
+                    switch (this.state.currentTutorialStep) {
+                      case 0:
+                        return (
+                          <Text style={{
+                            fontSize: 28, fontWeight: 'bold',
+                          }}>Bem-vindos à Fin<Text style={{ color: colors.verdeFinddo }}>dd</Text>o!</Text>);
+                      case 1:
+                        return (
+                          <Text style={{
+                            fontSize: 28, fontWeight: 'bold',
+                          }}>Informe o problema</Text>);
+                      case 2:
+                        return (
+                          <Text style={{
+                            fontSize: 22, fontWeight: 'bold',
+                            textAlign: 'center'
+                          }}>O profissional irá informar o valor do serviço e só o realizará em caso de aprovação.</Text>);
+                      case 3:
+                        return (
+                          <Text style={{
+                            fontSize: 22, fontWeight: 'bold',
+                            textAlign: 'center'
+                          }}>Todos os nossos profissionais parceiros são <Text style={{ color: colors.verdeFinddo }}>qualificados</Text>, verificados e oriundos de indicação.</Text>);
+                      default:
+                        return (null);
+                    }
+                  })()}
+                </View>
+                <View style={{
+                  height: 320, width: '100%',
+                  flexDirection: 'row',
+                  alignItems: 'center', justifyContent: 'center'
+                }}>
+                  {(() => {
+                    if (this.state.currentTutorialStep > 0) {
+                      return (
+                        <TouchableOpacity
+                          onPress={() => {
+                            this.setState({ currentTutorialStep: this.state.currentTutorialStep - 1 });
+                          }}
+                          style={{
+                            height: '100%', width: 40,
+                            alignItems: 'center', justifyContent: 'center',
+                          }}>
+                          <Icon
+                            style={{ width: 35 }}
+                            name={'keyboard-arrow-left'}
+                            size={35} color='gray' />
+                        </TouchableOpacity>
+                      );
+                    } else {
+                      return (
+                        <View style={{
+                          height: '100%', width: 40,
+                          alignItems: 'center', justifyContent: 'center',
+                        }}></View>
+                      );
+                    }
+                  })()}
+                  <View style={{
+                    height: '100%', width: 240,
+                    justifyContent: 'flex-end', alignItems: 'center'
+                  }}>
+                    {(() => {
+                      switch (this.state.currentTutorialStep) {
+                        case 0:
+                          return (<SvgXml xml={passo0} width={200} height={200}></SvgXml>);
+                        case 1:
+                          return (<SvgXml xml={passo1} width={200} height={200}></SvgXml>);
+                        case 2:
+                          return (<SvgXml xml={passo2} width={200} height={200}></SvgXml>);
+                        case 3:
+                          return (<SvgXml xml={passo3} width={200} height={200}></SvgXml>);
+                        default:
+                          return (null);
+                      }
+                    })()}
+                    <View style={{ height: 40 }}></View>
+                    {(() => {
+                      switch (this.state.currentTutorialStep) {
+                        case 0:
+                          return (
+                            <Text style={{
+                              fontSize: 20, fontWeight: 'bold',
+                              textAlign: 'center'
+                            }}>Nunca foi tão <Text style={{ color: colors.verdeFinddo }}>fácil</Text> realizar uma manutenção em sua residência.</Text>);
+                        case 1:
+                          return (
+                            <Text style={{
+                              fontSize: 20, fontWeight: 'bold',
+                              textAlign: 'center'
+                            }}>e agende uma visita de nossos profissionais parceiros.</Text>);
+                        case 2:
+                          return (
+                            <Text style={{
+                              fontSize: 20, fontWeight: 'bold',
+                              textAlign: 'center'
+                            }}>O pagamento será feito no <Text style={{ color: colors.verdeFinddo }}>cartão</Text>.</Text>);
+                        case 3:
+                          return (
+                            <TouchableOpacity
+                              style={{
+                                alignItems: 'center', justifyContent: 'center',
+                                backgroundColor: colors.verdeFinddo, borderRadius: 20,
+                                width: 240, height: 45
+                              }}
+                              onPress={() => this.setState({ isShowingTutorial: false })}>
+                              <Text style={{ fontSize: 20, color: colors.branco }}>CONCLUIR</Text>
+                            </TouchableOpacity>
+                          );
+                        default:
+                          return (null);
+                      }
+                    })()}
+                  </View>
+                  {(() => {
+                    if (this.state.currentTutorialStep < 3) {
+                      return (
+                        <TouchableOpacity
+                          onPress={() => {
+                            this.setState({ currentTutorialStep: this.state.currentTutorialStep + 1 });
+                          }}
+                          style={{
+                            height: '100%', width: 40,
+                            alignItems: 'center', justifyContent: 'center',
+                          }}>
+                          <Icon
+                            style={{ width: 35 }}
+                            name={'keyboard-arrow-right'}
+                            size={35} color='gray' />
+                        </TouchableOpacity>
+                      );
+                    } else {
+                      return (
+                        <View style={{
+                          height: '100%', width: 40,
+                          alignItems: 'center', justifyContent: 'center',
+                        }}></View>
+                      );
+                    }
+                  })()}
+                </View>
+                <View style={{
+                  height: 80, width: 320,
+                  justifyContent: 'center', alignItems: 'center',
+                  flexDirection: 'row'
+                }}>
+                  <SvgXml xml={this.state.currentTutorialStep === 0 ? bolaCheia : bolaApagada} style={{ marginHorizontal: 10 }} width={15} height={15}></SvgXml>
+                  <SvgXml xml={this.state.currentTutorialStep === 1 ? bolaCheia : bolaApagada} style={{ marginHorizontal: 10 }} width={15} height={15}></SvgXml>
+                  <SvgXml xml={this.state.currentTutorialStep === 2 ? bolaCheia : bolaApagada} style={{ marginHorizontal: 10 }} width={15} height={15}></SvgXml>
+                  <SvgXml xml={this.state.currentTutorialStep === 3 ? bolaCheia : bolaApagada} style={{ marginHorizontal: 10 }} width={15} height={15}></SvgXml>
+                </View>
               </View>
             </View>
           </Modal>
