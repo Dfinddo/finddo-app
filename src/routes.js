d4f658b130073232435f16fd3f80a657c81cc22a
user166642 <user166642@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2020-04-10 20:54:03 -0400

    - Correcao da pagina de login

--- src/routes.js
+++ src/routes.js
@@ -194,7 +194,7 @@ export default class App extends Component {
     super(props);
     // ============================ ATENÇÃO ============================
     // ================ NUNCA COMMITAR O ID DA APLICAÇÃO ===============
-    OneSignal.init(productionConfig.oneSignalApiKey);
+    OneSignal.init(developConfig.oneSignalApiKey);
 
     OneSignal.addEventListener('received', this.onReceived);
     OneSignal.addEventListener('opened', this.onOpened);
