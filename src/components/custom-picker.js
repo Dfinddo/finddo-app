5b74c904611b84804628a54cefb5cf857e5502c2
Matheus Perrut <matheus.perrut2302@gmail.com>
2020-04-14 15:31:56 -0300

    - Custom Picker: versão 1 concluída

--- src/components/custom-picker.js
+++ src/components/custom-picker.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
-import { View, TouchableOpacity, Text, Modal } from 'react-native';
+import {
+  View, TouchableOpacity,
+  Text, Modal,
+  StyleSheet,
+  FlatList
+} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { colors } from '../colors';
 
 /**
+ * Objetos para o array precisam ter `content`, `value` e `id`,
+ * ambos com o tipo `String`, `id` precisa ser único
+ * 
  * Exemplo de uso
  * ```
- * <CustomPicker style={{height: 100, backgroundColor: 'white'}} items={[{ content: 'Teste' }]} />
+ * const items = [{ content: 'teste', value: 'teste', id: '0' }];
+ * 
+ * const onSelect = (item) => console.log(item);
+ * 
+ * <CustomPicker 
+ *   style={{height: 100, backgroundColor: 'white'}} 
+ *   items={items} defaultItem={items[0]}
+ *   onSelect={onSelect} />
  * ```
  */
 export class CustomPicker extends Component {
@@ -15,20 +31,24 @@ export class CustomPicker extends Component {
 
   state = {
     items: [],
-    selectedItem: { content: 'Selecione um opção', value: null, id: 0 },
+    selectedItem: { content: 'Selecione uma opção', value: null, id: 0 },
     isSelectingItem: false
   };
 
   componentDidMount() {
-    let id = 0;
-    const itemsArray = this.props.items
-      .map((item) => {
-        item.id = String(id);
-        id++;
-        return item;
-      });
+    this.setState({ items: this.props.items }, () => {
+      if (this.props.defaultItem) {
+        this.setState({ selectedItem: this.props.defaultItem });
+      } else {
+        this.setState({ selectedItem: this.state.items[0] });
+      }
+    });
+  }
 
-    this.setState({ items: itemsArray });
+  onItemSelected = (selectedItem) => {
+    this.setState({ isSelectingItem: false, selectedItem }, () => {
+      this.props.onSelect(selectedItem);
+    });
   }
 
   render() {
@@ -39,22 +59,66 @@ export class CustomPicker extends Component {
           transparent={true}
           visible={this.state.isSelectingItem && this.state.items.length > 0}
         >
-          <View>
-
+          <View style={[
+            customPickerStyles.modalSelecaoContainer,
+            customPickerStyles.centralizar]}>
+            <FlatList
+              style={customPickerStyles.listaDeItems}
+              data={this.state.items}
+              renderItem={({ item }) => (
+                <Item
+                  id={item.id}
+                  item={item}
+                  onPress={this.onItemSelected}
+                />
+              )}
+              keyExtractor={item => item.id}
+            />
           </View>
         </Modal>
         <TouchableOpacity
-          style={{
-            flex: 1, flexDirection: 'row',
-            alignItems: 'center', justifyContent: 'space-between'
-          }}>
-          <Text style={{ marginLeft: 10 }}>{this.state.selectedItem.content}</Text>
+          style={customPickerStyles.botaoPrincipalSelectContainer}
+          onPress={() => this.setState({ isSelectingItem: true })}>
+
+          <Text style={customPickerStyles.rotuloSelecionado}>
+            {this.state.selectedItem.content}
+          </Text>
           <Icon
-            style={{ width: 35 }}
+            style={customPickerStyles.setaDropdown}
             name={'keyboard-arrow-down'}
-            size={25} color='gray' />
+            size={25} color={colors.cinza} />
+
         </TouchableOpacity>
       </View>
     );
   }
+}
+
+const customPickerStyles = StyleSheet.create({
+  centralizar: { alignItems: 'center', justifyContent: 'center' },
+  botaoPrincipalSelectContainer: {
+    flex: 1, flexDirection: 'row',
+    alignItems: 'center', justifyContent: 'space-between'
+  },
+  rotuloSelecionado: { marginLeft: 10 },
+  setaDropdown: { width: 35 },
+  modalSelecaoContainer: {
+    flex: 1, backgroundColor: colors.transparenciaModais,
+  },
+  itemSelecionavelContainer: {
+    width: 320, height: 50,
+    backgroundColor: colors.branco, marginVertical: 10
+  },
+  listaDeItems: { marginVertical: 20, flex: 1 }
+});
+
+export default function Item(props) {
+  return (
+    <TouchableOpacity style={[
+      customPickerStyles.itemSelecionavelContainer,
+      customPickerStyles.centralizar]}
+      onPress={() => props.onPress(props.item)}>
+      <Text style={{ fontSize: 18 }}>{props.item.content}</Text>
+    </TouchableOpacity>
+  );
 }
