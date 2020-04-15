f9cff6d65164e203a2fe50aa15dc867d5781bfb0
Matheus Perrut <matheus.perrut2302@gmail.com>
2020-04-15 15:35:48 -0300

    - Utilização em Meus Pedidos Profissional

--- src/pages/profissional_servicos/meus-pedidos-profissional.js
+++ src/pages/profissional_servicos/meus-pedidos-profissional.js
@@ -3,7 +3,7 @@ import {
   Text, FlatList,
   StyleSheet, TouchableOpacity,
   View, ImageBackground,
-  Picker, ActivityIndicator,
+  ActivityIndicator,
   Modal, Alert,
   RefreshControl
 } from 'react-native';
@@ -13,6 +13,17 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import { colors } from '../../colors';
 import { NavigationEvents } from 'react-navigation';
 import VisualizarPedidoProfissional from '../../components/modal-visualizar-pedido-profissional';
+import { CustomPicker } from '../../components/custom-picker';
+
+const estadoPedidoValues = [
+  { content: 'Selecione um status', value: 'none', id: '0' },
+  { content: 'Pedido em Análise', value: 'analise', id: '1' },
+  { content: 'Agendando Visita', value: 'agendando_visita', id: '2' },
+  { content: 'Profissional à Caminho', value: 'a_caminho', id: '3' },
+  { content: 'Serviço em Execução', value: 'em_servico', id: '4' },
+  { content: 'Concluído', value: 'finalizado', id: '5' },
+  { content: 'Cancelado', value: 'cancelado', id: '6' },
+];
 
 const enumEstadoPedidoMap = {
   analise: 'Pedido em Análise',
@@ -37,7 +48,7 @@ export default class MeusPedidosProfissional extends Component {
     pedidosServico: [],
     pedidosFinalizado: [],
     pedidosCancelado: [],
-    tipoPedidoSelecionado: Object.keys(enumEstadoPedidoMap)[6],
+    tipoPedidoSelecionado: estadoPedidoValues[0].value,
     loadingData: false,
     pedidoCorrente: null,
     enderecoSelecionado: null,
@@ -197,21 +208,11 @@ export default class MeusPedidosProfissional extends Component {
             alignItems: 'center', justifyContent: 'center',
             marginBottom: 10
           }}>
-            <Picker
-              selectedValue={this.state.tipoPedidoSelecionado}
-              style={{
-                height: 50, width: 320,
-              }}
-              onValueChange={(itemValue, itemIndex) =>
-                this.setState({ tipoPedidoSelecionado: itemValue })
-              }>
-              <Picker.Item label={Object.values(enumEstadoPedidoMap)[0]} value={Object.keys(enumEstadoPedidoMap)[0]} />
-              <Picker.Item label={Object.values(enumEstadoPedidoMap)[2]} value={Object.keys(enumEstadoPedidoMap)[2]} />
-              <Picker.Item label={Object.values(enumEstadoPedidoMap)[3]} value={Object.keys(enumEstadoPedidoMap)[3]} />
-              <Picker.Item label={Object.values(enumEstadoPedidoMap)[4]} value={Object.keys(enumEstadoPedidoMap)[4]} />
-              <Picker.Item label={Object.values(enumEstadoPedidoMap)[5]} value={Object.keys(enumEstadoPedidoMap)[5]} />
-              <Picker.Item label={Object.values(enumEstadoPedidoMap)[6]} value={Object.keys(enumEstadoPedidoMap)[6]} />
-            </Picker>
+            <CustomPicker
+              style={meusPedidosStyles.selecionaStatus}
+              items={estadoPedidoValues}
+              onSelect={({ value }) => this.setState({ tipoPedidoSelecionado: value })}
+            />
           </View>
           <View style={{ height: 425 }}>
             {(() => {
@@ -310,6 +311,9 @@ const meusPedidosStyles = StyleSheet.create({
     width: 340, borderRadius: 20,
     color: colors.branco, marginTop: 10,
     alignItems: 'center', justifyContent: 'center'
+  },
+  selecionaStatus: {
+    height: 50, width: 320,
   }
 });
 
