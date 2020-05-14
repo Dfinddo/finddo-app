6e1c171ba9d4d88e4b7b70c40805b0aa70597f10
Matheus Perrut <matheus.perrut2302@gmail.com>
2020-05-14 00:17:24 -0300

    - Implementação do serviço de recuperação de senha

--- src/pages/login/esqueci-senha-email.js
+++ src/pages/login/esqueci-senha-email.js
@@ -3,10 +3,13 @@ import {
   ImageBackground, View,
   Text, StyleSheet,
   TouchableOpacity,
-  TextInput, ScrollView
+  TextInput, ScrollView, Alert, ActivityIndicator
 } from 'react-native';
 import { colors } from '../../colors';
 import HeaderFundoTransparente from '../../components/header-fundo-transparente';
+import backendRails from '../../services/backend-rails-api';
+import { developConfig, productionConfig } from '../../../credenciais-e-configuracoes';
+import { StackActions } from 'react-navigation';
 
 export default class EsqueciSenhaEmail extends Component {
   static navigationOptions = {
@@ -17,34 +20,100 @@ export default class EsqueciSenhaEmail extends Component {
 
   state = {
     email: '',
+    isLoading: false
   };
+
+  enviarFormRecuperacaoDeSenha = (email) => {
+    this.setState({ isLoading: true }, () => {
+      backendRails.post('auth/password', { email, redirect_url: developConfig.backendUrl })
+        .then(_ => {
+          Alert.alert(
+            'Recuperação de Senha',
+            'Será enviado email com instruções de ' +
+            'recuperação se existir usuário associado ao email fornecido.',
+            [{
+              text: 'OK', onPress: () => {
+                const popAction = StackActions.pop({
+                  n: 1,
+                });
+                this.props.navigation.dispatch(popAction);
+              }
+            }]);
+        }).catch(error => {
+          if (error.response) {
+            /*
+             * The request was made and the server responded with a
+             * status code that falls out of the range of 2xx
+             */
+            Alert.alert(
+              'Recuperação de Senha',
+              'Será enviado email com instruções de ' +
+              'recuperação se existir usuário associado ao email fornecido.',
+              [{
+                text: 'OK', onPress: () => {
+                  const popAction = StackActions.pop({
+                    n: 1,
+                  });
+                  this.props.navigation.dispatch(popAction);
+                }
+              }])
+          } else if (error.request) {
+            /*
+             * The request was made but no response was received, `error.request`
+             * is an instance of XMLHttpRequest in the browser and an instance
+             * of http.ClientRequest in Node.js
+             */
+            Alert.alert(
+              'Falha ao executar a operação',
+              'Verifique sua conexão e tente novamente',
+              [
+                { text: 'OK', onPress: () => { this.setState({ isLoading: false }) } },
+              ],
+              { cancelable: false },
+            );
+          }
+        });
+    });
+  }
 
   render() {
     return (
       <ImageBackground
         style={this.esqueciSenhaEmailStyle.backgroundImageContent}
         source={require('../../img/Ellipse.png')}>
-        <ScrollView>
-          <View style={{ height: 220 }}></View>
-          <View style={this.esqueciSenhaEmailStyle.emailForm}>
-            <View style={this.esqueciSenhaEmailStyle.containerForm}>
-              <Text style={this.esqueciSenhaEmailStyle.fontTitle}>Esqueci minha senha</Text>
-              <TextInput
-                style={this.esqueciSenhaEmailStyle.formSizeAndFont}
-                onChangeText={text => { this.setState({ email: text }) }}
-                placeholder="Email"
-                keyboardType="email-address"
-                maxLength={11}
-                value={this.state.email}
-              />
-            </View>
-            <TouchableOpacity
-              style={this.esqueciSenhaEmailStyle.continuarButton}
-              onPress={() => { this.props.navigation.navigate('EsqueciSenhaNovaSenha'); }}>
-              <Text style={this.esqueciSenhaEmailStyle.continuarButtonText}>CONTINUAR</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
+        {(() => {
+          if (this.state.isLoading) {
+            return (
+              <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <ActivityIndicator size="large" color={colors.verdeFinddo} animating={true} />
+              </View>
+            );
+          } else {
+            return (
+              <ScrollView>
+                <View style={{ height: 220 }}></View>
+                <View style={this.esqueciSenhaEmailStyle.emailForm}>
+                  <View style={this.esqueciSenhaEmailStyle.containerForm}>
+                    <Text style={this.esqueciSenhaEmailStyle.fontTitle}>Esqueci minha senha</Text>
+                    <TextInput
+                      style={this.esqueciSenhaEmailStyle.formSizeAndFont}
+                      onChangeText={text => { this.setState({ email: text }) }}
+                      placeholder="Email"
+                      keyboardType="email-address"
+                      maxLength={100}
+                      value={this.state.email}
+                    />
+                  </View>
+                  <TouchableOpacity
+                    style={this.esqueciSenhaEmailStyle.continuarButton}
+                    onPress={() => { this.enviarFormRecuperacaoDeSenha(this.state.email) }}>
+                    <Text style={this.esqueciSenhaEmailStyle.continuarButtonText}>CONTINUAR</Text>
+                  </TouchableOpacity>
+                </View>
+              </ScrollView>
+            );
+          }
+        })()}
       </ImageBackground >
     );
   }
