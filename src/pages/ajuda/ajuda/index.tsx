4b8fd466131e646bbb8b7d741e412346fbe2ef53
Gabriel Silveira <gabriel.silveira@poli.ufrj.br>
2020-06-05 22:02:33 -0300

    Compose help page with UI Kitten

--- src/pages/ajuda/ajuda/index.tsx
+++ src/pages/ajuda/ajuda/index.tsx
@@ -1,136 +1,67 @@
-import React, {Component} from "react";
-import {
-	View, ImageBackground,
-	ScrollView, StyleSheet,
-	Linking, Alert,
-} from "react-native";
-import HeaderTransparenteSemHistorico from "../../../components/header-transparente-sem-historico";
+import React, {useState} from "react";
+import {ImageBackground, ScrollView, Linking, Alert} from "react-native";
 import {termos} from "../../cadastros/termos";
 import {sobre} from "../sobre-o-app";
 import {politica} from "../../cadastros/politica";
-import AccordianInfoApp from "../../../components/accordian-info-app";
-import AccordianInfoContato from "../../../components/accordian-info-contato";
+import {Layout, TabView, Tab, Text} from "@ui-kitten/components";
 
-export default class AjudaScreen extends Component {
+const openChat = (): void => {
 
-	public setState: any;
+	// TODO: remover esse telefone e colocar nos environments
+	const PHONE = 5521980503130;
+	const url = `whatsapp://send?text=&phone=${PHONE}`;
 
-	static navigationOptions = {
-		headerTransparent: true,
-		headerTitle: () => <HeaderTransparenteSemHistorico />,
-	};
+	Linking.openURL(url).catch(() => {
 
-	constructor(props) {
-
-		super(props);
-
-	}
-
-	state = {
-		faleConoscoIsOpened: false,
-		termosIsOpened: false,
-		sobreIsOpened: false,
-		politicaPrivacidadeIsOpened: false,
-	};
-
-	changeTermosIsOpenedState = () => {
-
-		const value = this.state.termosIsOpened;
-
-		this.setState({termosIsOpened: !value});
-
-	};
-
-	changePoliticaIsOpenedState = () => {
-
-		const value = this.state.politicaPrivacidadeIsOpened;
-
-		this.setState({politicaPrivacidadeIsOpened: !value});
-
-	}
-
-	changeSobreIsOpenedState = () => {
-
-		const value = this.state.sobreIsOpened;
-
-		this.setState({sobreIsOpened: !value});
-
-	};
-
-	changeFaleConoscoIsOpenedState = () => {
-
-		const value = this.state.faleConoscoIsOpened;
-
-		this.setState({faleConoscoIsOpened: !value});
-
-	};
-
-	openChat = () => {
-
-		// TODO: remover esse telefone e colocar nos environments
-		const url = "whatsapp://send?text=" + "" + "&phone=55" + "21980503130";
-
-		Linking.openURL(url).catch(_ => {
-
-			Alert.alert(
-				"Erro",
-				"Whatsapp não instalado no seu dispositivo",
-				[{text: "OK", onPress: () => { }}],
-				{cancelable: false},
-			);
-
-		});
-
-	};
-
-	public render() {
-
-		return (
-			<ImageBackground
-				style={this.ajudaScreenStyle.backgroundImageContent}
-				source={require("../../../img/Ellipse.png")}>
-				<View style={{height: 50}}></View>
-				<View style={{flex: 1}}>
-					<ScrollView>
-						<View style={{
-							alignItems: "center",
-							justifyContent: "center",
-							width: "100%",
-							height: 500,
-						}}>
-							<AccordianInfoApp
-								titulo={"Sobre o App"}
-								opened={this.state.sobreIsOpened}
-								onPress={() => this.changeSobreIsOpenedState()}
-								content={sobre}></AccordianInfoApp>
-							<View style={{height: 5}} />
-							<AccordianInfoApp
-								titulo={"Termos de uso"}
-								opened={this.state.termosIsOpened}
-								onPress={() => this.changeTermosIsOpenedState()}
-								content={termos}></AccordianInfoApp>
-							<View style={{height: 5}} />
-							<AccordianInfoApp
-								titulo={"Politica de privacidade"}
-								opened={this.state.politicaPrivacidadeIsOpened}
-								onPress={() => this.changePoliticaIsOpenedState()}
-								content={politica}></AccordianInfoApp>
-							<View style={{height: 5}} />
-							<AccordianInfoContato
-								opened={this.state.faleConoscoIsOpened}
-								onPress={() => this.changeFaleConoscoIsOpenedState()}
-								onPressAction={() => this.openChat()}>
-							</AccordianInfoContato>
-						</View>
-					</ScrollView>
-				</View>
-			</ImageBackground>
+		Alert.alert(
+			"Erro",
+			"Whatsapp não instalado no seu dispositivo",
+			[{text: "OK", onPress: () => void 0}],
+			{cancelable: false},
 		);
 
-	}
-
-	ajudaScreenStyle = StyleSheet.create({
-		backgroundImageContent: {width: "100%", height: "100%"},
 	});
 
+};
+
+// TODO: Add contact button
+export default function(): JSX.Element {
+
+	const [selectedIndex, setSelectedIndex] = useState(0);
+
+	return (
+		<ImageBackground
+			style={backgroundImageStyle}
+			source={require("../../../img/Ellipse.png")}>
+			<TabView
+				selectedIndex={selectedIndex}
+				onSelect={index => setSelectedIndex(index)}>
+				<Tab title="Sobre o App">
+					<Layout>
+						<ScrollView style={textContainerStyle}>
+							<Text>{sobre}</Text>
+						</ScrollView>
+					</Layout>
+				</Tab>
+				<Tab title="Termos de uso">
+					<Layout>
+						<ScrollView style={textContainerStyle}>
+							<Text>{termos}</Text>
+						</ScrollView>
+					</Layout>
+				</Tab>
+				<Tab title="Politica de privacidade">
+					<Layout>
+						<ScrollView style={textContainerStyle}>
+							<Text>{politica}</Text>
+						</ScrollView>
+					</Layout>
+				</Tab>
+			</TabView>
+		</ImageBackground>
+	);
+
 }
+
+const backgroundImageStyle = {width: "100%", height: "100%"};
+const textContainerStyle = {padding: 20, paddingTop: 0};
