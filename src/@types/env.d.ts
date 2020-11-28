1231963efae70550cca2219486a1c3dbb59a891b
Schlanger <leoschlanger@gmail.com>
2020-11-28 12:38:43 -0300

    onesignal initial configs

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,5 +1,6 @@
 declare module '@env' {
   export const ONE_SIGNAL_API_KEY: string;
+  export const ONE_SIGNAL_APP_ID: string;
   export const MOIP_CREDS_DATA_PUBLIC_KEY: string;
   export const BACKEND_URL: string;
   export const BACKEND_URL_STORAGE: string;
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable no-console */
+/* eslint-disable line-comment-position */
+/* eslint-disable no-inline-comments */
 import "react-native-gesture-handler";
 import "mobx-react-lite/batchingForReactNative";
 import React, {useEffect, FC} from "react";
@@ -12,12 +15,67 @@ import {finddoLightTheme, finddoDarkTheme} from "themes";
 import Routes from "routes";
 import {useUser} from "hooks";
 import {navigationRef} from "./routes/rootNavigation";
+import OneSignal from 'react-native-onesignal'; 
+import { ONE_SIGNAL_APP_ID } from "@env";
+
+interface ResultsNotification {
+		notification: {
+			payload: {
+				body: string;
+				additionalData: string;
+			};
+			isAppInFocus: boolean;
+		}	
+}
 
 const App: FC = () => {
 	const userStore = useUser();
 
+	function onReceived(notification: string ): void {
+    console.log("Notification received: ", notification);
+  }
+
+  function onOpened(openResult: ResultsNotification): void {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  function onIds(device: string): void {
+    console.log('Device info: ', device);
+  }
+
+	// function myiOSPromptCallback(permission){
+	// 		// do something with permission value
+	// }
+
 	useEffect(() => {
 		userStore.restoreSession();
+		
+		OneSignal.setLogLevel(6, 0);
+  
+		// Replace 'YOUR_ONESIGNAL_APP_ID' with your OneSignal App ID.
+		OneSignal.init(ONE_SIGNAL_APP_ID, {
+			kOSSettingsKeyAutoPrompt : false, 
+			kOSSettingsKeyInAppLaunchURL: false, 
+			kOSSettingsKeyInFocusDisplayOption:2
+		});
+
+		OneSignal.inFocusDisplaying(2); // Controls what should happen if a notification is received while the app is open. 2 means that the notification will go directly to the device's notification center.
+		
+		// The promptForPushNotifications function code will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step below)
+		// OneSignal.promptForPushNotificationsWithUserResponse(myiOSPromptCallback);
+
+		OneSignal.addEventListener('received', onReceived);
+		OneSignal.addEventListener('opened', onOpened);
+		OneSignal.addEventListener('ids', onIds);
+		
+		return () => {
+					OneSignal.removeEventListener('received', onReceived);
+					OneSignal.removeEventListener('opened', onOpened);
+					OneSignal.removeEventListener('ids', onIds);
+		}
 	}, [userStore]);
 
 	return (
